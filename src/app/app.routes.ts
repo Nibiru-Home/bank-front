9abbf1d9fd4da17d122b87fd78a7202574b68e3a
R4r4s <rngheru@gmail.com>
2026-01-13 16:51:56 +0100

    Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './components/pages/index/index';
 import { LoginComponent } from './components/pages/login/login';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent }
 ];
