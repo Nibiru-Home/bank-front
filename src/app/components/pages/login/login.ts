9cd119533858e6cb80abb4559165357361be6158
R4r4s <rngheru@gmail.com>
2026-01-13 13:18:16 +0100

    Imagen parte izquierda

---
+++ src/app/components/pages/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class LoginComponent {
+}
