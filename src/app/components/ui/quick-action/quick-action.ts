67ecbc425fb3a88a7f9aae81ffa9c0f56abe6eb6
R4r4s <rngheru@gmail.com>
2026-01-13 18:19:38 +0100

    modificaciones

--- src/app/components/ui/quick-action/quick-action.ts
+++ src/app/components/ui/quick-action/quick-action.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-quick-action',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './quick-action.html',
   styleUrl: './quick-action.scss'
 })
 export class QuickActionComponent {
   @Input() label: string = '';
   @Input() iconSrc: string = '';
+  @Input() href: string | null = null;
 }
