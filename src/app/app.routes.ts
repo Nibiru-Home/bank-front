9300ac4873ec847ae1c53e73847ea696f982e49f
ainhoa <ainhoamiret@gmail.com>
2026-01-14 19:20:40 +0100

    Mejorar ver tarjeta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AccountsComponent } from './components/pages/accounts/accounts';
 import { CardsComponent } from './components/pages/cards/cards';
 import { AccountDetailComponent } from './components/pages/account-detail/account-detail';
 import { CardDetailComponent } from './components/pages/card-detail/card-detail';
+import { ContratarProductosComponent } from './components/pages/contratar-productos/contratar-productos';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'accounts', component: AccountsComponent },
   { path: 'accounts/:id', component: AccountDetailComponent },
   { path: 'cards', component: CardsComponent },
-  { path: 'cards/:id', component: CardDetailComponent }
+  { path: 'cards/:id', component: CardDetailComponent },
+  { path: 'contratar-productos', component: ContratarProductosComponent }
 ];
