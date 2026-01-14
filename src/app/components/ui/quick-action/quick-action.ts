c7eceabe482af852ecc51abec41bb49474a0d2a6
R4r4s <rngheru@gmail.com>
2026-01-14 13:01:59 +0100

    feat(Nibiru-Home/sprints#45): contratar-productos

--- src/app/components/ui/quick-action/quick-action.ts
+++ src/app/components/ui/quick-action/quick-action.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-quick-action',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './quick-action.html',
   styleUrl: './quick-action.scss'
 })
@@ -11,4 +12,5 @@ export class QuickActionComponent {
   @Input() label: string = '';
   @Input() iconSrc: string = '';
   @Input() href: string | null = null;
+  @Input() routerLink: string | null = null;
 }
