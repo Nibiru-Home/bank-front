1cab079a0c37278652d380ecdec675b33905b50c
R4r4s <rngheru@gmail.com>
2026-01-15 22:20:37 +0100

    feat(Nibiru-Home/sprints#45): Pagina contratar

--- src/app/components/pages/contratar-productos/contratar-productos.ts
+++ src/app/components/pages/contratar-productos/contratar-productos.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../layout/header/header';
 import { FooterComponent } from '../../layout/footer/footer';
 
 @Component({
   selector: 'app-contratar-productos',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [RouterLink, HeaderComponent, FooterComponent],
   templateUrl: './contratar-productos.html',
   styleUrl: './contratar-productos.scss'
 })
