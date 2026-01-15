061a35419a3f80faadb7bf6f9094905941f1e616
R4r4s <rngheru@gmail.com>
2026-01-15 10:32:18 +0100

    fix(Rutas)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './components/pages/index/index';
 import { LoginComponent } from './components/pages/login/login';
 import { AccountsComponent } from './components/pages/accounts/accounts';
+import { AccountCreateComponent } from './components/pages/account-create/account-create';
 import { CardsComponent } from './components/pages/cards/cards';
+import { CardCreateComponent } from './components/pages/card-create/card-create';
 import { AccountDetailComponent } from './components/pages/account-detail/account-detail';
 import { CardDetailComponent } from './components/pages/card-detail/card-detail';
 import { ContratarProductosComponent } from './components/pages/contratar-productos/contratar-productos';
@@ -12,8 +14,10 @@ export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'login', component: LoginComponent },
   { path: 'accounts', component: AccountsComponent },
+  { path: 'accounts/new', component: AccountCreateComponent },
   { path: 'accounts/:id', component: AccountDetailComponent },
   { path: 'cards', component: CardsComponent },
+  { path: 'cards/new', component: CardCreateComponent },
   { path: 'cards/:id', component: CardDetailComponent },
   { path: 'contratar-productos', component: ContratarProductosComponent },
   { path: 'incidencias', component: IncidenciasComponent }
